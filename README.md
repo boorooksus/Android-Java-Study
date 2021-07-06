89ad1ff1b6dcd58fae30f10d807e678b4c723b00
boorooksus <boorooksus@gmail.com>
2021-07-06 12:28:50 +0900

    프로젝트 생성 및 LinearLaout 사용

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
-"# Android-Java-Study" 
+# Android-Java-Study
 
 
 
 자바를 이용한 안드로이드 프로그래밍 공부 자료
+
---
+++ app/src/main/java/com/example/s01_hello_android/MainActivity.java
@@ -0,0 +1,18 @@
+package com.example.s01_hello_android;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    TextView textView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        textView =  findViewById(R.id.textView);
+        textView.setText("hi");
+    }
+}
