abde3453178d07b3c0236bc5f6a9a5589bb6b799
boorooksus <boorooksus@gmail.com>
2021-07-08 10:19:27 +0900

    단위 계산기

---
+++ S02_Unit_Converter/app/src/main/java/com/example/s02_unit_converter/MainActivity.java
@@ -0,0 +1,58 @@
+package com.example.s02_unit_converter;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Switch;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    EditText editText;
+    TextView textView;
+    Button button;
+    Switch sw;
+    ImageView image;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        textView = findViewById(R.id.textView);
+        button = findViewById(R.id.button);
+        editText = findViewById(R.id.editTextNumberSigned);
+        sw = findViewById(R.id.switch1);
+        image = findViewById(R.id.imageView);
+
+        button.setOnClickListener(new View.OnClickListener(){
+            @SuppressLint("SetTextI18n")
+            @Override
+            public void onClick(View view){
+                String strC = editText.getText().toString();
+                double c = Double.parseDouble(strC);
+
+                if (strC.equals(""))
+                    return;
+                if(sw.isChecked()){
+                    double f = 9 / 5.0 * c + 32;
+                    textView.setText("" + f);
+                    image.setImageResource(R.drawable.java);
+                }
+                else{
+
+                    double f = (c - 32) * 5.0 / 9;
+                    textView.setText("" + f);
+                    image.setImageResource((R.drawable.android));
+                }
+
+            }
+
+        });
+
+    }
+}
