e2f5b9f5e74cfe02b70b257b779bfd8bf9fdddb6
boorooksus <boorooksus@gmail.com>
2021-07-08 23:30:22 +0900

    암시적 Intent

--- S03_Multi_Activities/app/src/main/java/com/example/s03_multi_activities/SecondActivity.java
+++ S03_Multi_Activities/app/src/main/java/com/example/s03_multi_activities/SecondActivity.java
@@ -2,15 +2,34 @@ package com.example.s03_multi_activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
 public class SecondActivity extends AppCompatActivity {
+    Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
+
+        button = findViewById(R.id.buttonWeb);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // 웹 여는 intent
+                Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("http://www.naver.com"));
+
+                // 지도 여는 intent(위도, 경도 정보 필요)
+//                Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("geo:37.44960881406647, 126.6531570043012"));
+
+                startActivity(intent);
+            }
+        });
     }
 
     public void backHandler(View view){
