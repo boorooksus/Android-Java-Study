de4692bfc0d969d786dcd0d0f2408cdcb8ff2994
boorooksus <boorooksus@gmail.com>
2021-07-08 23:13:42 +0900

    Multi Activities

---
+++ S03_Multi_Activities/app/src/main/java/com/example/s03_multi_activities/SecondActivity.java
@@ -0,0 +1,20 @@
+package com.example.s03_multi_activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class SecondActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+    }
+
+    public void backHandler(View view){
+        finish();
+    }
+
+}
